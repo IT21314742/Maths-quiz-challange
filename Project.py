a3f70c67f2411649bed8961a70196c8d539a6a82
IT21314742 <it21314742@my.sliit.lk>
2024-08-02 18:51:40 +0530

    Update
    Update

--- Project.py
+++ Project.py
@@ -8,7 +8,7 @@ def generate_problem():
     left = random.randint(MIN_OPERAND, MAX_OPERAND)
     right = random.randint(MIN_OPERAND, MAX_OPERAND)
 
-    expr = str(left) + " " + ope + " " + str(operator)
+    expr = str(left) + " " + oper + " " + str(operator)
     print(expr)
     return expr
 
