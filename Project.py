0bee5f8cb78dc6d96eee750ab351cc0ec1d4bc05
IT21314742 <it21314742@my.sliit.lk>
2024-08-05 23:36:40 +0530

    Update
    Update

--- Project.py
+++ Project.py
@@ -6,7 +6,7 @@ MAX_OPERAND = 12
 TOTAL_PROBLEMS = 10
 
 def generate_problem():
-    left = rando.randint(MIN_OPERAND, MAX_OPERAND)
+    left = random.randint(MIN_OPERAND, MAX_OPERAND)
     right = random.randint(MIN_OPERAND, MAX_OPERAND)
 
     expr = str(left) + " " + operator + " " + str(right)
