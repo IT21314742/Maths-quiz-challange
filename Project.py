b26ea41625798250ec7beb64f3cf61b8c9bfe206
IT21314742 <it21314742@my.sliit.lk>
2024-08-06 13:18:54 +0530

    Update
    Update

--- Project.py
+++ Project.py
@@ -15,6 +15,6 @@ def generate_problem():
 
 for i in range(TOTAL_PROBLEMS):
     expr, answer = generate_problem()
-    print("Problem #" + str(i +1) + ":" + expr )
+    guessprint("Problem #" + str(i +1) + ":" + expr )
 
 
