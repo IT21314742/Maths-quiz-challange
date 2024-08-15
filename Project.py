69a5d90805ee276f5d6247001306aa46b96d3781
IT21314742 <it21314742@my.sliit.lk>
2024-08-15 09:32:47 +0530

    Update
    Update

--- Project.py
+++ Project.py
@@ -30,7 +30,7 @@ for i in range(TOTAL_PROBLEMS):
         wrong += 1
 
 end_time = time.time()
-total_time = end_time - 
+total_time = end_time - start_time
 
 print("----------")
 print("Nice work!")
